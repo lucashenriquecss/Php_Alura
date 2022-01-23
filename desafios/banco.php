db76e8ed078784cf01d21f8efdaa5d7330b4c9db
Lucas Henrique Calheiros Saraiva Santos <43302698+lucashenriquecss@users.noreply.github.com>
2022-01-23 13:18:28 -0300

    update conta banco

--- desafios/banco.php
+++ desafios/banco.php
@@ -1,36 +1,59 @@
 <?php
 
-exibeMenssagem{
-    
-}
-
-$contacorrente = [
-    '102.66' => [
+$contasCorrentes = [
+    '123.456.789-10' => [
         'titular' => 'Maria',
-        'Saldo' => 499
+        'saldo' => 10000
     ],
-    '103.66' => [
-        'titular' => 'Marcos',
-        'Saldo' => 10000
+    '123.456.689-11' => [
+        'titular' => 'Alberto',
+        'saldo' => 300
     ],
-    '104.666' => [
-        'titular' => 'Pedro',
-        'Saldo' => 10000
-    ],
-    '105.66' => [
-        'titular' => 'João',
-        'Saldo' => 10000
-    ],
+    '123.256.789-12' => [
+        'titular' => 'Vinicius',
+        'saldo' => 100
+    ]
 ];
 
+function exibeMensagem($mensagem){
+     echo $mensagem . PHP_EOL;
+    }
 
+
+function depositar(array $conta, float $valorADepositar): array
+{
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Depositos precisam ser positivos");
+    }
+    return $conta;
+} 
+
+function sacar(array $conta, float $valorASacar): array
+{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não tem saldo suficiente");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+
+    return $conta;
+}
+
+/*
 if (500 > $contacorrente['102.66']['Saldo']) {
     echo 'Você nao pode sacara esse valor';
     # code...
 }else {
     $contacorrente['102.66']['Saldo'] -= 500;
 }
+*/
+$contasCorrentes['123.456.689-11'] = depositar(
+    $contasCorrentes['123.456.689-11'],
+    900
+);
 
-foreach($contacorrente as $cpf => $conta){
-    echo $cpf . " " . $conta['titular'] . ' ' . $conta['Saldo'] .PHP_EOL;
-};
+foreach ($contasCorrentes as $cpf => $conta) {
+    exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
+}
